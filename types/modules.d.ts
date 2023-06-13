d3ed30b3f916e2f70dcc7ab37e56beea7431c379
Nícolas Carvalho <nicolas.mnw@gmail.com>
2023-06-13 15:32:53 -0300

    removido a dependencia do gulp

--- types/modules.d.ts
+++ types/modules.d.ts
@@ -1,39 +1,9 @@
-declare module '*.html' {
-    const value: any;
-    export default value;
-}
-
-declare module '*.jpg' {
-    const value: any;
-    export default value;
-}
-
 declare module '*.glb' {
     const value: any;
     export default value;
 }
 
-declare module '*.jpeg' {
-    const value: any;
-    export default value;
-}
-
 declare module '*.png' {
-    const value: any;
-    export default value;
-}
-
-declare module '*.gif' {
-    const value: any;
-    export default value;
-}
-
-declare module '*.vert' {
-    const value: any;
-    export default value;
-}
-
-declare module '*.frag' {
     const value: any;
     export default value;
 }
